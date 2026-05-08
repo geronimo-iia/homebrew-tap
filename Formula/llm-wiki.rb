b8a3cd79aafb2d218eaff3fa15926e5cca4e51a5
geronimo-iia <352507+geronimo-iia@users.noreply.github.com>
2026-05-08 04:58:55 +0000

    chore: bump llm-wiki to 0.4.1

--- Formula/llm-wiki.rb
+++ Formula/llm-wiki.rb
@@ -1,26 +1,26 @@
 class LlmWiki < Formula
   desc "Git-backed wiki engine with MCP server"
   homepage "https://github.com/geronimo-iia/llm-wiki"
-  version "0.4.0"
+  version "0.4.1"
   license "MIT OR Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/geronimo-iia/llm-wiki/releases/download/v0.4.0/aarch64-apple-darwin.tar.gz"
-      sha256 "97d9092187efb3eb8ab50b314ba1ea6d68405c82a0ff2a7f3c199e73f539a012"
+      url "https://github.com/geronimo-iia/llm-wiki/releases/download/v0.4.1/aarch64-apple-darwin.tar.gz"
+      sha256 "12bea267ab0681ea184277a1c8cd48a2d1f97994ac3939f024d2956d1cb2b34f"
     else
-      url "https://github.com/geronimo-iia/llm-wiki/releases/download/v0.4.0/x86_64-apple-darwin.tar.gz"
-      sha256 "a314081a8d662cfd66f1e4524ecfa61017a7e7ba381d232ad530c96bb9b66eaa"
+      url "https://github.com/geronimo-iia/llm-wiki/releases/download/v0.4.1/x86_64-apple-darwin.tar.gz"
+      sha256 "9a3eb669455125231f28232a3b9a9bd6d7e1f9141e7d34c52ffaf60fa0c64452"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/geronimo-iia/llm-wiki/releases/download/v0.4.0/aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "20930d638b9f870aa4becbe6937893c6af2cdda8b125b77b26bbdfe9d7b9e21e"
+      url "https://github.com/geronimo-iia/llm-wiki/releases/download/v0.4.1/aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "648d7216050d279537fa013b9bee9e70330db36fbf5c501192f668a3ed1663b4"
     else
-      url "https://github.com/geronimo-iia/llm-wiki/releases/download/v0.4.0/x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "083c410aec3b6443e4c57377c26e5a3e33ff1f9dea28c49ff97f3b5f46bee772"
+      url "https://github.com/geronimo-iia/llm-wiki/releases/download/v0.4.1/x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "54428519e677d3f7605d8a366e145c0a20d556e577d1bea2b90168a505a51886"
     end
   end
 
